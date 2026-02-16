4a2a87284bca60f7f824d599f845916439e683cc
dinaKSari <dinakumala7070@gmail.com>
2026-02-16 19:14:42 +0700

    membuat route name

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,15 @@ Route::get('/user/{name?}', function ($name='John') {
 return 'Nama saya '.$name;
 });
 
+//route nama
+Route::get('/user/profile', function () {
+ //
+})->name('profile');
+Route::get(
+ '/user/profile',
+ [UserProfileController::class, 'show']
+)->name('profile');
+// Generating URLs...
+$url = route('profile');
+// Generating Redirects...
+return redirect()->route('profile');
