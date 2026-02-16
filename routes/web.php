309e918f9fac3ca1d229d9326e8ca22853fdc7d9
dinaKSari <dinakumala7070@gmail.com>
2026-02-16 18:36:21 +0700

    membuat route / dan about

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
 });
 Route::get('/hello', function () {
  return 'Hello World';
 });
 Route::get('/world', function () {
  return 'World';
+});
+Route::get('/about', function () {
+ return 'NIM 244107020072, Nama Dina Kumala Sari';
 });
